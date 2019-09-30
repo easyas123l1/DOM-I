3b32d41c88c601a1cfde05e970231aaad6acd9ef
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-09-30 14:23:34 -0500

    added src tags and nav a tags textContent

--- js/index.js
+++ js/index.js
@@ -40,3 +40,18 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaimg = document.getElementById('cta-img');
+ctaimg.src = siteContent['cta']['img-src'];
+
+let middleimg = document.getElementById('middle-img');
+middleimg.src = siteContent['main-content']['middle-img-src'];
+
+let a = document.querySelectorAll('a');
+a[0].textContent = siteContent['nav']['nav-item-1'];
+a[1].textContent = siteContent['nav']['nav-item-2'];
+a[2].textContent = siteContent['nav']['nav-item-3'];
+a[3].textContent = siteContent['nav']['nav-item-4'];
+a[4].textContent = siteContent['nav']['nav-item-5'];
+a[5].textContent = siteContent['nav']['nav-item-6'];
+
